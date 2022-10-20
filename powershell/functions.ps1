2c6def836ca24f731af13c94975140cb8752fbb0
Arne Halvorsen <arne.halvorsen@gmail.com>
2022-10-20 18:32:36 +0200

    wip: ready to start play online on github

--- powershell/functions.ps1
+++ powershell/functions.ps1
@@ -6,20 +6,37 @@ function New-Entry {
 
     [string]$FileName = (($Title.Split([IO.Path]::GetInvalidFileNameChars()) -join '_' -replace ' ','_') + '.md')
 
-    [string]$Date = (Get-Date -Format 'yyyyMMdd')
-
-    Write-Host $FileName
-    Write-Host $Date
+    [string]$Date = (Get-Date -Format 'yyyyMMdd hh:mm')
 
     if (-not (Test-Path -Path './entries')) {
         mkdir 'entries'
     }
     Push-Location 'entries'
 
-        ('### ' + $Title) | Out-File -FilePath $FileName 
+        ('[TOC (README)](../README.md)') | Out-File -FilePath $FileName  
+        ('### ' + $Title) | Out-File -Append -FilePath $FileName 
         ($Date) | Out-File -Append -FilePath $FileName 
         (' ') | Out-File -Append -FilePath $FileName 
 
     Pop-Location
-    
+}
+
+function Update-Readme {
+
+    $Readme = 'README.md'
+    ('') | Out-File -FilePath $Readme
+
+    $EntryFiles = (Get-ChildItem -Path 'entries' | Where-Object { -not $_.PsIsContainer } | Sort-Object LastWriteTime -Descending)
+    foreach ($File in $EntryFiles) {
+        Write-Host ('Processing ' + $File.Name)
+
+        $Content = Get-Content -Path $File.FullName
+        $Title = $Content[1].Substring(4)
+        $Date = $Content[2].Trim()
+        Write-Host ('  Title: ' + $Title)
+        Write-Host ('  Date: ' + $Date)
+
+        ($Date + ' **[' + $Title + '](./entries/' + $File.Name + ')**') | Out-File -FilePath $Readme -Append
+
+    }
 }
